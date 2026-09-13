d62f821eddb4796e6cf83c6fa56160abcc7efe05
Ivan Ivashchenko <62796151+IvashchenkoIvanIgorevich@users.noreply.github.com>
2026-09-13 15:07:23 +0300

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+
+        int left = 1;
+        int right = piles[piles.length - 1];
+
+        while(left <= right) { // TODO
+            
+            int middle = (left + right) / 2;
+            int rate = getHours(piles, middle);
+
+            if (rate > h) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        return left;
+    }
+
+    private int getHours(int[] piles, int middle) {
+        int result = 0;
+        
+        for(int i:piles) {
+            result += (int) Math.ceil((double) i / middle);
+        }
+        return result;
+    }
+}
