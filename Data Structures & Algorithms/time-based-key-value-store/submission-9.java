a8f68b14b930c8bd26d6d5ac2f9f0a63314a6762
Ivan Ivashchenko <62796151+IvashchenkoIvanIgorevich@users.noreply.github.com>
2026-09-18 13:54:39 +0300

    Add: time-based-key-value-store - submission-9

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-9.java
@@ -0,0 +1,45 @@
+class TimeMap {
+    Map<String, Map<Integer, String>> vault;
+
+    public TimeMap() {
+        vault = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        if (vault.containsKey(key)) {
+            vault.get(key).put(timestamp, value);
+        } else {
+            Map<Integer, String> n = new HashMap<>();
+            n.put(timestamp, value);
+            vault.put(key, n);
+        }
+    }
+
+    public String get(String key, int timestamp) {
+        Map<Integer, String> map = vault.get(key);
+        if(map == null) return "";
+        if (map.containsKey(timestamp)) return map.get(timestamp);
+
+        int[] keys = map.keySet().stream().mapToInt(Integer::intValue).toArray();
+        if (keys.length == 1) {
+            return keys[0] <= timestamp ? map.get(keys[0]) : "";
+        }
+        Arrays.sort(keys);
+        
+        int l = 0;
+        int r = keys.length - 1;
+        while (l <= r) {
+            int m = (l+r)/2;
+            if(keys[m] >= timestamp) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        // System.out.println(keys[r]);
+        if (r < 0) return "";
+        return keys[r] <= timestamp ? map.get(keys[r]) : "";
+    }
+}
+// 5,10 -> 0
+//
